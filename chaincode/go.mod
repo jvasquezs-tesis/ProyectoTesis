35ad746387127c404e48607322c394502b8c5ae9
jvasquezs <jonathan.vasquez@usach.cl>
2020-11-17 00:17:01 -0300

    Agregar nuevos archivos y actualizacion de smart contract

---
+++ chaincode/go.mod
@@ -0,0 +1,5 @@
+module github.com/jvasquezs-tesis/ProyectoTesis/tree/master/chaincode/minvucontrol
+
+go 1.13
+
+require github.com/hyperledger/fabric-contract-api-go/contractapi v1.1.0
--- chaincode/minvucontrol/minvucontrol.go
+++ chaincode/minvucontrol/minvucontrol.go
@@ -1,10 +1,7 @@
-package chaincode
+package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"strconv"
-
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
@@ -12,16 +9,43 @@ type SmartContract struct{
 	contractapi.Contract
 }
 
+
+//Definir un activo 
+type Postulacion struct{
+	Puntaje string `json:"puntaje"`       
+	Rut string  `json:"rut"`
+}
+
+func (s *SmartContract) Set (ctx contractapi.TransactionContextInterface, postulacionId string, puntaje string, rut string){
+//agregar validaciones de sintaxis
+
+	postulacion := Postulacion{
+		Puntaje: puntaje,
+		Rut: rut
+	}
+
+	//pasar estructura a bytes
+	postulacionAsBytes, _ := json.Marshal(postulacion)
+	if err != nil{
+		fmt.Printf("3 Marshal error", err.Error())
+		return err
+	}
+
+	// permite almacenar en el libro distribuido
+	return ctx.GetStub().PutState(postulacionId,postulacionAsBytes)
+
+}
+
 func main(){
 	chaincode, err := contractapi.newChaincode(new(SmartContract))
 
 	if err != nil{
-		fmt.Printf("Error 1 crear minvucontrol:", err.Error())
+		fmt.Printf("Error 1 crear minvucontrol chaincode:", err.Error())
 		return
 	}
 
 	if err:= chaincode.Start(); err != nil{
-		fmt.Printf("Error 2 crear minvucontrol:", err.Error())
+		fmt.Printf("Error 2 crear minvucontrol chaincode:", err.Error())
 	}
 
 }
